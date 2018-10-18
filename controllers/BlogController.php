214e9401ced0901356a9e582687a987a54a89cbb
tangchunwei <2239991414@qq.com>
2018-10-18 09:05:52 +0800

    添加搜索排序

--- controllers/BlogController.php
+++ controllers/BlogController.php
@@ -27,8 +27,32 @@ class BlogController
     // 显示文章列表
     public function index()
     {
+        // 拼接搜索语句
+        $where=1;
+        if(isset($_GET['keywords'])&&$_GET['keywords']){
+            $where.=" and (title like '%{$_GET['keywords']}%' or content like '%{$_GET['keywords']}%')";
+        }
+        if(isset($_GET['start_date'])&&$_GET['start_date']){
+            $where.=" and created_at >= '{$_GET['start_date']}'";
+        }
+        if(isset($_GET['updated_at'])&&$_GET['updated_at']){
+            $where.=" and updated_at <= '{$_GET['updated_at']}' ";
+        }
+        if(isset($_GET['is_show'])&&$_GET['is_show']!=''){
+            $where.=" and is_show ={$_GET['is_show']}";
+        }
+        // 拼接排序语句
+        $orderBy="created_at";
+        $orderWay='asc';
+        if(isset($_GET['order_by'])&&$_GET['order_by']=='display'){
+            $orderBy='display';
+        }
+        if(isset($_GET['order_way'])&&$_GET['order_way']=='desc'){
+            $orderWay='desc';
+        }
+        // echo $orderBy, $orderWay;
         $blog = new Blog;
-        $blogs = $blog->get("select * from blogs");
+        $blogs = $blog->get("select * from blogs where $where order by $orderBy $orderWay");
         view("blogs.index", [
             'blogs' => $blogs
         ]);
--- public/index.php
+++ public/index.php
@@ -9,13 +9,14 @@ function load($class)
 }
 spl_autoload_register("load");
 // 实现view视图显示函数
-function view($file,$data=[]){
+function view($file, $data = [])
+{
     // 如果传了数据，就把数组展开成变量
-    if($data){
+    if ($data) {
         extract($data);
     }
     // 加载试图 
-    require_once ROOT.'views/'.str_replace('.','/',$file).'.html';
+    require_once ROOT . 'views/' . str_replace('.', '/', $file) . '.html';
 }
 // 解析路由，以及任务分发
 function route()
@@ -23,36 +24,47 @@ function route()
     //获取 URL 
     $url = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '/';
     // 定义默认的控制器以及方法
-    $defautController='Indexcontroller';
-    $defaultAction='index';
-    if($url=='/'){
+    $defautController = 'Indexcontroller';
+    $defaultAction = 'index';
+    if ($url == '/') {
         return [
             $defautController,
             $defaultAction,
         ];
     }
     // 判断是否是根路径
-    else if(strpos($url,'/',1)!==false){
+    else if (strpos($url, '/', 1) !== false) {
         // 去掉第一个/
-        $url=ltrim($url,'/');
+        $url = ltrim($url, '/');
         // 把url转成数组
-        $route=explode('/',$url);
+        $route = explode('/', $url);
         // 格式化控制器
-        $route[0]=ucfirst($route[0]).'Controller';
+        $route[0] = ucfirst($route[0]) . 'Controller';
         return $route;
 
-    }
-    else{
+    } else {
         die("请求的路径不合法");
     }
 }
 // 跳转方法
-function redirect($url){
-    header("location:".$url);
+function redirect($url)
+{
+    header("location:" . $url);
     exit();
 }
-$route=route();
-$controller="controllers\\$route[0]";
-$action=$route[1];
-$_C=new $controller;
+// 获取url参数
+function getUrlParams($except = [])
+{
+    $ret = '';
+    foreach ($_GET as $k => $v) {
+        if(!in_array($k,$except)){
+            $ret .= "&$k=$v";
+        }
+    }
+    return $ret;
+}
+$route = route();
+$controller = "controllers\\$route[0]";
+$action = $route[1];
+$_C = new $controller;
 $_C->$action();
